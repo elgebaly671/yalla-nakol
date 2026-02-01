e117c11a1de64557db6d0ef8118a45e6b2c2a26f
elgebaly671 <abdelrhman.elgebaly@student.guc.edu.eg>
2026-02-01 16:05:11 +0200

    testing git

---
+++ yalla-nakol/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>}></Route>
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
